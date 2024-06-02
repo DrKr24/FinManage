c671d55e237a511be4be2cb9d4ef0b28fc5f9626
Krashiy <dimitrykravchenko2000@gmail.com>
2024-06-02 16:22:54 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   base: "/FinManage/",
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "pages/*",
+          dest: "pages",
+        },
+      ],
+    }),
+  ],
+  build: {
+    outDir: "dist",
+  },
 });
